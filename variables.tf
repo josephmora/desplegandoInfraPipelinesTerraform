05e10f317d1ccaa3cf9469f894922fd447577a5d
Nicholas Klick <nklick@gitlab.com>
2020-05-05 23:30:22 -0400

    add initial files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {}
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "az" {}
+variable "ssh_key_name" {}
+variable "vpc_cidr_block" {}
+variable "dmz_cidr_block" {}
+variable "main_instance_private_ip" {}
