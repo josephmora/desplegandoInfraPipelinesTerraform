06a24c0ab10da0ce9cb17514b128b0d45e9e1b7a
J. Daniel Medeiros <medeiros@enta.pt>
2021-01-12 00:08:33 +0000

    Added aws_session_token variable and rewrote Terraform v0.13 - Explicit Provider Source Locations

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 2.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
-  version = "~> 2.0"
+  token = var.token
 }
 
 terraform {
-  backend "http" {}
+  backend "http" {
+    
+  }
 }
