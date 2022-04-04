25cd00de1ef8fed85fd3b8787048a6155372f7e7
Claudio Cesar Sanchez Tejeda <claudio.sanchez.tejeda@digitalhouse.com>
2022-04-04 17:32:53 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
+echo "Antes configurar la variable de entorno GITLAB_TOKEN:"
+echo "export GITLAB_TOKEN=<SU_TOKEN_DE_GITLAB>"
+
 terraform init \
-    -backend-config="address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>" \
-    -backend-config="lock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>/lock" \
-    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>/lock" \
-    -backend-config="username=<YOUR-USERNAME>" \
-    -backend-config="password=<YOUR-ACCESS-TOKEN>" \
+    -backend-config="address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws" \
+    -backend-config="lock_address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws/lock" \
+    -backend-config="unlock_address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws/lock" \
+    -backend-config="username=demonccc" \
+    -backend-config="password=$GITLAB_TOKEN" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
     -backend-config="retry_wait_min=5"
